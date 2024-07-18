3ed12bf8397a655cef63b62f4f9aa48f5faef7bc
R-Diogo <118007741+R-Diogo@users.noreply.github.com>
2024-07-17 23:10:43 -0300

    Update utils.py

--- DeepQLearning/utils.py
+++ DeepQLearning/utils.py
@@ -24,3 +24,42 @@ def reward_engineering_car_racing(action, reward):
         reward += 1.5
 
     return reward
+
+def test_nn(agent, env, n, stack_frames = 3, skip_frames = 3):
+    done = False
+    total_return_history, cumulative_return_history = [], []
+
+    for episodes in range(1, n+1):
+        # Reset the environment
+        state, _ = env.reset()
+        state = preprocess_state(state)
+        state_stack = deque([state]*stack_frames, maxlen=stack_frames)
+        
+        total_reward = 0.0
+        cumulative_reward = 0.0
+        done = False
+        
+        for time in range(1, 180):
+            curr_state_stack = process_deque(state_stack)
+            action = agent.act(curr_state_stack)
+
+            reward = 0
+            for _ in range(skip_frames+1):
+                next_state, r, terminated, truncated, _ = env.step(action)
+                reward += r
+                done = terminated or truncated
+                if done:
+                    break
+
+            total_reward += reward
+            cumulative_reward = agent.gamma * cumulative_reward + reward
+
+            next_state = preprocess_state(next_state)
+            state_stack.append(next_state)
+
+            if done or time == 179:
+                print('Episode: {}/{}, Scores(Time Frames): {}, Total Rewards(adjusted): {:.2}, Cumulative Rewards: {:.2}'.format(episodes, n, time, float(total_reward), float(cumulative_reward)))
+                total_return_history.append(total_reward)
+                cumulative_return_history.append(cumulative_reward)
+                break
+    return total_return_history, cumulative_return_history
